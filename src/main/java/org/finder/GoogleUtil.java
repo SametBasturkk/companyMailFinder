7fdd0ca5e92d35c076b4d8784d12708cdcb37786
Samet <sametklou1907@gmail.com>
2024-09-13 12:14:57 +0300

    u

--- src/main/java/org/finder/GoogleUtil.java
+++ src/main/java/org/finder/GoogleUtil.java
@@ -1,11 +1,14 @@
 package org.finder;
 
+import generator.RandomUserAgentGenerator;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
-import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 public class GoogleUtil {
 
@@ -16,13 +19,23 @@ public class GoogleUtil {
         return googleSearch(company);
     }
 
-    private static String googleSearch(String company) throws IOException, InterruptedException {
-        String url = "https://www.google.com/search?q=" + URLEncoder.encode(company, "UTF-8");
-        HttpResponse resp = client.send(HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build(), HttpResponse.BodyHandlers.ofString());
+    private static String googleSearch(String company) throws IOException {
+        Document doc = Jsoup.connect("https://www.google.com/search?q=" +
+                URLEncoder.encode(company, "UTF-8")).userAgent(RandomUserAgentGenerator.getNextNonMobile()).get();
 
-        return resp.body().toString();
+        Elements results = doc.select("a[href]");
+        ArrayList<String> urls = new ArrayList<>();
+        for (var result : results) {
+            urls.add(result.attr("href"));
+        }
 
+        for (String url : urls) {
+            if (url.startsWith("/url?") && !url.contains("google")) {
+                System.out.println(url.substring(7));
+            }
+        }
+
+
+        return "test";
     }
 }
